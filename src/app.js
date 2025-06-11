9afab17f2243c8670cc98db224c3a69c3713b096
Vaishnavi Pandit <vaishnavipandit62153@gmail.com>
2025-06-11 11:46:35 +0530

    add libraries

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 let express=require("express");
 let mysql=require("mysql2");
+let bodyparser=require("body-parser");
+let cookieParser=require("cookie-parser");
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(cookieParser());
+app.set("view engine","ejs");
+app.use(express.static("public"));
 let app=express();
 
 module.exports=app;
